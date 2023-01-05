45a493589870546029313c5e05b0e79b24a7893f
avakimc <avakimcohen@gmail.com>
2023-01-04 20:48:38 -0600

    Removed a few lines of unused code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,15 +5,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.Crusher;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.libs.XboxController;
 
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.Crusher;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -22,7 +19,6 @@ import frc.robot.subsystems.Crusher;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  private static RobotContainer INSTANCE;
   // The robot's subsystems and commands are defined here...
   public final XboxController controller;
 
