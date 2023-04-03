0a1280c7efa2dabd5903ffbf26bee7cad8cf7496
avakimc <109251157+avakimc@users.noreply.github.com>
2023-04-03 08:30:04 -0500

    Ava/structure (#3)
    * Added instant commands and subsystem objects
    
    * Added XboxController lib, fixed crusher code, added robot map value
    
    * Removed a few lines of unused code
    
    * Added drive command, and made drive functions not static
    
    * Moved drive motor IDs to robotmap
    
    ---------
    
    Co-authored-by: avakimc <avakimcohen@gmail.com>

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
@@ -7,27 +9,25 @@ public class DriveTrain extends SubsystemBase {
 
 
 // identifying the type of motor and giving each on a number that is defined somewhere else
-    private static VictorSPX motorLeft1 = new VictorSPX(1);
-    private static VictorSPX motorLeft2 = new VictorSPX(2);
-    private static VictorSPX motorRight1 = new VictorSPX(3);
-    private static VictorSPX motorRight2 = new VictorSPX(4);
+    private static VictorSPX motorLeft1 = new VictorSPX(RobotMap.DRIVE_LEFT_MOTOR_1_ID);
+    private static VictorSPX motorLeft2 = new VictorSPX(RobotMap.DRIVE_LEFT_MOTOR_2_ID);
+    private static VictorSPX motorRight1 = new VictorSPX(RobotMap.DRIVE_RIGHT_MOTOR_1_ID);
+    private static VictorSPX motorRight2 = new VictorSPX(RobotMap.DRIVE_RIGHT_MOTOR_2_ID);
 
     // double means the same thing as float in python
     // setting how the speed should be interpreted the left ones are negative because they are backwards in the physical robot
-    public static void setLeftMotors(double speed) {
+    public void setLeftMotors(double speed) {
         motorLeft1.set(ControlMode.PercentOutput, -speed);
         motorLeft2.set(ControlMode.PercentOutput, -speed);
     }
 
-    public static void setRightMotors(double speed) {
+    public void setRightMotors(double speed) {
         motorRight1.set(ControlMode.PercentOutput, speed);
         motorRight2.set(ControlMode.PercentOutput, speed);
     }
     // makes wheels spin using methods above
-    public static void move(double left, double right){
+    public void move(double left, double right){
         setLeftMotors(left);
         setRightMotors(right);
     }
-
-    
 }
